2590cafb67c889b7878a01b9e240e8676cb081b6
amandaoktivia14 <mandasharfina@gmail.com>
2023-09-06 13:44:39 +0700

    update

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s^=_voy0=5zefb3slqf&&l^p^)r9#$w5xrepglfsr%*f_@mjx^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
